9f12c9ba4f908063f4919465a684ad271ce76097
Fray93 <fredyhernandez93@gmail.com>
2021-11-22 02:19:53 -0500

    Actualizacion HU 15

--- DATABASE-GROWTHDEV/index.js
+++ DATABASE-GROWTHDEV/index.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const url = 'mongodb+srv://userpro:Projecto4@gd.g0cij.mongodb.net/';
 const client = new MongoClient(url);
@@ -19,7 +19,7 @@ async function main() {
         email: "sofiacdlmxd@gmail.com",
         pwd: "sofia18",
         id: 985664,
-        roll: "administrador",
+        rol: "administrador",
         estado: "pendiente"
     }); */
 
@@ -41,7 +41,7 @@ async function main() {
     console.log('HU 04 - Usuarios registrados:', consultaUsuarios); */
 
 
-    //HU 05 Comando para cambiar el estado del usuario en el proyecto:
+    //HU 05 Comando para cambiar el estado del usuario:
 
     /* const actualizarUsuario = await collection.updateOne({ id: 985664 }, { $set: { estado: "autorizado" } }); */
 
@@ -53,16 +53,77 @@ async function main() {
 
 
     //HU 07 Comando para aprobar la creacion de un proyecto:
-    /* const actualizarProyecto = await collection1.updateOne({ id: '' }, { $set: { estado1: "autorizado" } }); */
+
+    /* const actualizarProyecto = await collection1.updateOne({{"_id": ObjectId("619b414d14f20f9bbc59515e")}, { $set: { estado1: "autorizado" } }); */
 
 
     //HU 08 Comando para cambiar el estado del proyecto:
-    /* const actualizarProyecto = await collection1.updateOne({ id: ''  }, { $set: { estado2: "activo" }}); */
+
+    /* const actualizarProyecto = await collection1.updateOne({"_id": ObjectId("619b414d14f20f9bbc59515e")}, { $set: { estado2: "activo" }}); */
 
 
     //HU 09 Comando para cambiar la fase de un proyecto:
-    /* const actualizarProyecto = await collection1.updateOne({ id: '' }, { $set: { fase: "desarrollo/terminado" } }); */
 
+    /* const actualizarProyecto = await collection1.updateOne({"_id": ObjectId("619b414d14f20f9bbc59515e")}, { $set: { fase: "desarrollo/terminado" } }); */
+
+
+    //HU 10  Comando para ver la lista de estudiantes registrados en la plataforma:
+
+    /* const consultaUsuarios = await collection.find({ rol: "estudiante" }).toArray();
+    console.log('HU 10 - Usuarios rol estudiante:', consultaUsuarios); */
+
+
+    //HU 11 Comando para aceptar estudiante:
+
+    /* const actualizarUsuario = await collection.updateOne({ id: 985664 }, { $set: { estado: "autorizado/pendiente" } }); */
+
+
+    //HU 12 Comando para crear un nuevo proyecto:
+
+    /* const insertarProyecto = await collection1.insertOne({
+        nombre: "Prueba",
+        obj_g: "Objetivo General Prueba",
+        obj_s: "Objetivo Especificos Prueba",
+        psto: 1000000,
+        id: 6572,
+        lider: "Juan",
+        estado1: "pendiente",
+        estado2: "inactivo",
+        fase: "desarrollo",
+        solicitudes: {
+
+        }
+    }); */
+
+
+    //HU 13 Comando para listar proyectos a cargo:
+
+    /* const consultaProyectos = await collection1.find({id:6572}).toArray();
+    console.log('HU 13 - Proyectos registrados a cargo:', consultaProyectos);  */
+
+
+    //HU 14 Comando para actualizar datos de proyectos a cargo:
+
+    /* const actualizarProyecto = await collection1.updateOne({"_id": ObjectId("619b414d14f20f9bbc59515e")},
+        {
+            $set: {
+                nombre: "Cambio Prueba",
+                obj_g: "Objetivo General Cambio Prueba",
+                obj_s: "Objetivo Especificos Cambio Prueba",
+                psto: 1500000
+            }
+        }); */
+
+
+    //HU 15 Comando para listar las solicitudes realizadas por los estudiantes:
+
+    /* const consultaProyectos = await collection1.find().toArray();
+    console.log('HU 15 - Solicitudes registradas:', consultaProyectos); */
+
+
+    //HU 16 Comando para cambiar el estado a la solicitudes:
+
+    
 
     return 'done';
 
